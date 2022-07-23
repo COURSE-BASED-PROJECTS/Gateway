d3258023f7943d99d84216e67a9fa760b1450ff6
nhthong2001 <nhthong30@gmail.com>
2022-07-23 16:01:55 +0700

    add new config

--- src/main/java/com/up/Gateway/GatewayConfig.java
+++ src/main/java/com/up/Gateway/GatewayConfig.java
@@ -1,7 +1,5 @@
 package com.up.Gateway;
 
-import com.up.Gateway.filter.AuthFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,13 +8,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-    @Autowired
-    private AuthFilter filter;
+//    @Autowired
+//    private AuthFilter filter;
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("account-service", r -> r.path("/api/account/**").uri("http://localhost:9091"))
+                .route("account-service", r -> r.path("/auth/**").uri("http://localhost:9091"))
                 .route("driver-service", r -> r.path("/api/driver/**").uri("http://localhost:9092"))
                 .route("request-service", r -> r.path("/api/request/**").uri("http://localhost:9093"))
                 .route("taxi-service", r -> r.path("/api/taxi/**").uri("http://localhost:9094"))
